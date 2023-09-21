1c9c709a0d4450822dc59d2c62d20af914957e2a
apsinghrana100 <apsinghrana100@gmail.com>
2023-09-21 18:27:16 +0530

    data_insert_table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function ProductCategory() {
+
+}
+
+
+function ProductRow({ value }) {
+
+  console.log(value)
+  value.forEach(element => {
+    console.log(element)
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+      <tr>
+        <td>apple</td>
+        <td>manogo</td>
+      </tr>
+    </>
+  )
+}
+
+
+function ProductTable() {
+  return (
+    <>
+      <div className='product-table'>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+              <tr>
+                <th>Fruits</th>
+              </tr>
+            <tr>
+              <td>apple               1</td>
+            </tr>
+            <tr>
+              <td>manogo              1</td>
+            </tr>
+          </tbody>
+
+        </table>
+
+      </div>
+
+    </>
+  )
+}
+
+function SearchBar() {
+  return (
+    <>
+      <form>
+        <div className='searchBar'>
+          <input type="text" placeholder="Search.." />
+          <label>
+            <input type='checkbox' />
+            Only show product in Stock
+          </label>
+        </div>
+      </form>
+      <ProductTable />
+    </>
+  )
+}
+
+function FilterProductTable() {
+
+  return (
+    <div className="main-container">
+      <SearchBar />
     </div>
+
+  )
+
+}
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+];
+
+export default function App() {
+  return (
+    <FilterProductTable />
   );
 }
 
-export default App;
+// export default App = (()=>{
+//   return (
+//          <FilterProductTable />
+//       );
+// });
+
+
